c315941f1087589711d0191d2be91a43dd7d1444
Grayson Stanton <gstanton@unomaha.edu>
2019-05-17 11:38:54 -0500

    initial commit

---
+++ GitTest85/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest85
+//
+//  Created by Grayson Stanton on 5/17/19.
+//  Copyright © 2019 Grayson Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
